81186030b0f08af6dd1a6de42066bfbc66c1b1d5
Garrett Kelly <garrettk@subpop.com>
2015-08-11 16:12:09 -0700

    Adding a few more scripts, including gistit

---
+++ start.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+osascript -e 'tell application "NiceCast" to start broadcast '
+osascript -e 'tell application "iTunes" to play';
+
+osascript -e "tell application \"iTunes\" to set sound volume to 10";
+osascript -e "tell application \"iTunes\" to set sound volume to 20";
+osascript -e "tell application \"iTunes\" to set sound volume to 30";
+osascript -e "tell application \"iTunes\" to set sound volume to 40";
+osascript -e "tell application \"iTunes\" to set sound volume to 50";
+osascript -e "tell application \"iTunes\" to set sound volume to 60";
+osascript -e "tell application \"iTunes\" to set sound volume to 70";
+osascript -e "tell application \"iTunes\" to set sound volume to 80";
+osascript -e "tell application \"iTunes\" to set sound volume to 90";
+osascript -e "tell application \"iTunes\" to set sound volume to 100";
